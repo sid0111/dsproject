b62a03878b5b7360c6555301d409bea4f78c428d
Naik Siddhesh <naiksiddhesh110@gmail.com>
2025-10-30 12:29:00 +0530

    model training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -92,6 +92,7 @@ class DataTransformation:
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
+            logging.info(f"Saved processing object")
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessing_obj
